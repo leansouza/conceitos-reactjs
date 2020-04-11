54a2fbd317050c19c16192335476194efd743137
Lean Santana <leanderson.souza@safeticket.com.br>
2020-04-11 08:51:40 -0300

    Atualização do arquivo App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() =>{
+    api.get('repositories').then(reponse => {
+      setRepositories(reponse.data)
+    })
+  }, []);
+
   async function handleAddRepository() {
     // TODO
+    const response = await api.post('repositories', {
+      title: `Aplicativo React Feito em  - ${Date.now()}`,
+      url : "leanderson.com.br",
+      techs: ["Nodejs", "PHP", "Python"]
+    })
+
+    setRepositories([ ...repositories, response.data ])
+
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    await api.delete(`repositories/${id}`);
+
+  
+    setRepositories(repositories.filter(
+      reposit => reposit.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+        {repositories.map(reposit => (
+        <li key={reposit.id}>
+          {reposit.title}
+            <button onClick={() => handleRemoveRepository(reposit.id)}>
+              Remover
+            </button>
         </li>
+
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
